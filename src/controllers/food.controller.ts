dfc94060fcc5ed3eadb0e3cfb917090362e3c1a4
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-03-06 18:55:00 +0800

    all data

--- src/controllers/food.controller.ts
+++ src/controllers/food.controller.ts
@@ -15,8 +15,18 @@ export const fixFood: RequestHandler = async (req, res) => {
   try {
     const { foodId } = req.params;
     const categoryData = req.body;
-    await foodModel.create(categoryData);
-    res.status(201).json({ message: "Successfully fixed Food" });
+    const updateCategory = await foodModel.updateOne(
+      { _id: "67bfe5bf0063d4a717497470" },
+      {
+        $set: {
+          image:
+            "https://res.cloudinary.com/ds6kxgjh0/image/upload/v1739950347/q7dt7vhlctno2wmebov4.png",
+        },
+      }
+    );
+    res
+      .status(201)
+      .json({ message: "Successfully fixed Food", updateCategory });
   } catch (error) {
     res.status(500).json({ message: "Error in fixFood", error });
   }
