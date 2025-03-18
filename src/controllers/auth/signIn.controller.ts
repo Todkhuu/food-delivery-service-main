cf593ea8f1c1bb31b4767cb70117d33688167bd5
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-03-18 12:11:23 +0800

    generally

--- src/controllers/auth/signIn.controller.ts
+++ src/controllers/auth/signIn.controller.ts
@@ -7,26 +7,26 @@ export const signIn: RequestHandler = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email) {
-      res.status(401).json({ message: "Email oruulna uu" });
+      res.status(401).json({ message: "Email oruulna uu", error: true });
       return;
     }
 
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      res.status(404).json({ message: "burtgelgui HEREGLEGCH BN" });
+      res.status(404).json({ message: "Unregistered user!", error: true });
       return;
     }
 
     const isCorrect = compareSync(password, user.password);
 
     if (!isCorrect) {
-      res.status(401).json({ message: "password buruu" });
+      res.status(401).json({ message: "Password is wrong!", error: true });
       return;
     }
 
-    res.status(200).json({ message: "amjilttai nevterlee", data: user });
+    res.status(200).json({ message: "Successfully logged in", data: user });
   } catch (error) {
-    res.status(500).json({ message: "Error in getSignInUser" });
+    res.status(500).json({ message: "Error in getSignInUser", error });
   }
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { signUp } from "../controllers/auth/signUp.controller";
 import { signIn } from "../controllers/auth/signIn.controller";
+import { refresh } from "../controllers/auth/refresh.controller";
 
 const userRoute = Router();
 
 userRoute.post("/signup", signUp);
 userRoute.post("/signin", signIn);
+userRoute.get("/refresh", refresh);
 
 export default userRoute;
